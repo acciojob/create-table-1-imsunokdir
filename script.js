3047a72d18f1cbcf88be2081ee2f98ead4fb3edb
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-22 12:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+let row = 3;
 function insert_Row() {
     //Write your code here
-  
-  
+	let cell = 2;
+	const table = document.getElementById("sampleTable");
+	let newTr = document.createElement("tr");
+	for(let i=1;i<=cell;i++){
+		let newTd = document.createElement("td");
+		newTd.textContent = `Row${row} cell${i}`;
+		newTr.appendChild(newTd);
+	}
+	row++;
+	table.appendChild(newTr);
+
 }
